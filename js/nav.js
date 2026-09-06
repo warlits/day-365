15d397e17abf43fe5955649a257b2c6a3f5d4ceb
warlits <carlbanag1010@gmail.com>
2026-09-06 17:54:23 +0800

    Improved nav UX. Initiated page orientation. Next page fxn in progress

--- js/nav.js
+++ js/nav.js
@@ -2,3 +2,14 @@ const menuToggle = document.getElementById("menu-toggle");
 menuToggle.addEventListener('change', () => {
     document.body.classList.toggle('menu-open', menuToggle.checked);
 });
+
+const items = document.querySelectorAll('.item');
+items.forEach((item) => {
+    item.addEventListener('click', () => {
+        items.forEach(otherTiles => otherTiles.classList.remove('active'));
+        item.classList.add('active');
+
+        document.body.classList.remove('menu-open');
+        menuToggle.checked = false;
+    })
+} )
